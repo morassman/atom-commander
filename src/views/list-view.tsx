071f246cced709367f2eb612db10428f0890c653
morassman <morassman@gmail.com>
2021-04-19 20:31:11 +0200

    Fix page up and down

--- src/views/list-view.tsx
+++ src/views/list-view.tsx
@@ -9,14 +9,8 @@ import { SymLinkController } from '../controllers/symlink-controller'
 import { DirectoryView } from './directory-view'
 import { FileView } from './file-view'
 import { SymLinkView } from './symlink-view'
-import { VItem } from '../fs'
-import { ItemController } from '../controllers/item-controller'
 import { TBody } from './element-view'
 import { ItemView } from './item-view'
-// const ListFileView = require('./list-file-view')
-// const ListDirectoryView = require('./list-directory-view')
-// const ListSymLinkView = require('./list-symlink-view')
-// const ContainerView = require('./container-view')
 
 type BodyViewRefs = {
   
@@ -45,13 +39,13 @@ type BodyViewRefs = {
 }
 
 class BodyView extends View<Props, BodyViewRefs> {
-  constructor(private requestFocus: ()=>void) {
+  constructor(private requestFocus: ()=>void, private onBlur: ()=>void) {
     super({}, true)
   }
 
   render() {
     return <div ref='scroller' className='atom-commander-list-view-scroller' onClick={() => this.requestFocus()}>
-      <table ref='table' className='atom-commander-list-view-table' attributes={{tabindex:-1}}>
+      <table ref='table' className='atom-commander-list-view-table' attributes={{tabindex:-1}} onBlur={() => this.onBlur()}>
         <thead>
           <tr>
             <th ref='nameHeader' onClick={() => main.actions.sortByName()}>
@@ -102,30 +96,13 @@ export class ListView extends ContainerView {
 
   constructor(left: boolean) {
     super({left})
-    this.body = new BodyView(() => this.requestFocus())
+    this.body = new BodyView(() => this.requestFocus(), () => this.refreshHighlight())
     this.initialize()
   }
 
   container(): BodyView {
     return this.body
   }
-
-  // static container() {
-  //   // @div {class: 'atom-commander-list-view-resizer', click:'requestFocus', outlet: 'listViewResizer'}, =>
-  //   return this.div({class: 'atom-commander-list-view-scroller', outlet:'scroller', click:'requestFocus'}, () => {
-  //     return this.table({class: 'atom-commander-list-view-table', outlet: 'table'}, () => {
-  //       return this.tbody({class: 'atom-commander-list-view list', tabindex: -1, outlet: 'tableBody'})
-  //   })
-  // })
-  // }
-
-  // initialize(state){
-  //   super.initialize(state)
-
-  //   return this.tableBody.focusout(() => {
-  //     return this.refreshHighlight()
-  //   })
-  // }
 
   clearItemViews() {
     this.body.refs.tableBody.clear()
@@ -183,25 +160,24 @@ export class ListView extends ContainerView {
   }
 
   pageAdjust(up: boolean) {
-    // TODO
-    // if (!this.highlightedIndex || (this.itemViews.length === 0)) {
-    //   return
-    // }
+    if ((this.highlightedIndex === null) || (this.itemViews.length === 0)) {
+      return
+    }
 
-    // const itemViewHeight = this.body.refs.tableBody.height() / this.itemViews.length
+    const itemViewHeight = this.body.refs.tableBody.element.clientHeight / this.itemViews.length
 
-    // if (itemViewHeight === 0) {
-    //   return
-    // }
+    if (itemViewHeight === 0) {
+      return
+    }
 
-    // const scrollHeight = this.body.refs.scroller.scrollBottom() - this.body.refs.scroller.scrollTop()
-    // const itemsPerPage = Math.round(scrollHeight / itemViewHeight)
+    const scrollHeight = this.body.refs.scroller.clientHeight
+    const itemsPerPage = Math.round(scrollHeight / itemViewHeight)
 
-    // if (up) {
-    //   this.highlightIndex(this.highlightedIndex - itemsPerPage)
-    // } else {
-    //   this.highlightIndex(this.highlightedIndex + itemsPerPage)
-    // }
+    if (up) {
+      this.highlightIndex(this.highlightedIndex - itemsPerPage)
+    } else {
+      this.highlightIndex(this.highlightedIndex + itemsPerPage)
+    }
   }
 
   adjustContentHeight(change: number) {}
